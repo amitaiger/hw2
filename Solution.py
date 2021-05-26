614b4db77c74f8816392dfad472b5e02b850ed07
amitaiger <82775079+amitaiger@users.noreply.github.com>
2021-05-26 14:30:38 +0300

    Add files via upload

--- Solution.py
+++ Solution.py
@@ -80,7 +80,6 @@ def createTables():
     except Exception as e:
         print(e)
     finally:
-        # will happen any way after try termination or exception handling
         conn.close()
 
 
@@ -94,24 +93,18 @@ def clearTables():
         conn.execute(query)
         conn.commit()
     except DatabaseException.ConnectionInvalid as e:
-        # do stuff
         print(e)
     except DatabaseException.NOT_NULL_VIOLATION as e:
-        # do stuff
         print(e)
     except DatabaseException.CHECK_VIOLATION as e:
-        # do stuff
         print(e)
     except DatabaseException.UNIQUE_VIOLATION as e:
-        # do stuff
         print(e)
     except DatabaseException.FOREIGN_KEY_VIOLATION as e:
-        # do stuff
         print(e)
     except Exception as e:
         print(e)
     finally:
-        # will happen any way after code try termination or exception handling
         conn.close()
 
 
@@ -127,24 +120,18 @@ def dropTables():
         conn.execute(query)
         conn.commit()
     except DatabaseException.ConnectionInvalid as e:
-        # do stuff
         print(e)
     except DatabaseException.NOT_NULL_VIOLATION as e:
-        # do stuff
         print(e)
     except DatabaseException.CHECK_VIOLATION as e:
-        # do stuff
         print(e)
     except DatabaseException.UNIQUE_VIOLATION as e:
-        # do stuff
         print(e)
     except DatabaseException.FOREIGN_KEY_VIOLATION as e:
-        # do stuff
         print(e)
     except Exception as e:
         print(e)
     finally:
-        # will happen any way after code try termination or exception handling
         conn.close()
 
 
